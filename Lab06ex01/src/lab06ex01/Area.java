0b71e85eec32f8646ee3eb338227de0e833827e4
João Paulo <120557257+nullifidianz@users.noreply.github.com>
2023-09-22 22:54:11 -0300

    adição do ex6

---
+++ Lab06ex01/src/lab06ex01/Area.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab06ex01;
+
+/**
+ *
+ * @author jota
+ */
+public class Area {
+    
+    float l, h;
+    
+    public float calculaArea(float l){
+        return l*l;
+    }
+    
+    public float calculaArea(float l, float h){
+        return l*h;
+    }
+    
+}
---
+++ Lab06ex01/src/lab06ex01/Lab06ex01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab06ex01;
+
+/**
+ *
+ * @author jota
+ */
+public class Lab06ex01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Area a1 = new Area();
+    
+        
+        System.out.println("Area quadrado: " + a1.calculaArea(2));
+        System.out.println("Area quadrado: " + a1.calculaArea(2, 6));
+    }
+    
+}
